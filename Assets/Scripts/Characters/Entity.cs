5dbde1f02426fb700c7b6aeac0dedc9863e87fc2
Norbert Hańderek <morph.webdev@gmail.com>
2023-11-18 19:17:43 +0100

    Enemy health bar + cooldown moved to entity base

--- Assets/Scripts/Characters/Entity.cs
+++ Assets/Scripts/Characters/Entity.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
 {
-    private int _maxHealth;
-    private int _currentHealth;
+    private Dictionary<DamageDealerType, float> _damageCooldownDict;
+    private const float _damageCooldownDuration = 1.0f; // in seconds
+
+    protected float _maxHealth;
+    protected float _currentHealth;
     
     public enum DamageDealerType
     {
         Acid, Bullet, Knife
     }
 
-    public int Health
-    {
-        get { return _currentHealth; }
-        set { _currentHealth = value;  }
-    }
-    
-    public int MaxHealth
-    {
-        get { return _maxHealth; }
-        set { _maxHealth = value;  }
-    }
+    public float Health => _currentHealth;
 
-    public Entity(int currentHealth, int maxHealth)
+    public float MaxHealth => _maxHealth;
+
+    protected Entity(int currentHealth, int maxHealth)
     {
         _currentHealth = currentHealth;
         _maxHealth = maxHealth;
     }
+    
+    private void Start()
+    {
+        _damageCooldownDict = new Dictionary<DamageDealerType, float>();
+    }
 
     public void DamageEntity(int damage, DamageDealerType damageDealerType)
     {
-        if (_currentHealth <= 0 || !checkIfVulnerable(damageDealerType)) return;
+        if (_currentHealth <= 0 || !CheckIfVulnerable(damageDealerType)) return;
         _currentHealth -= damage;
             
         if (_currentHealth <= 0)
@@ -40,6 +40,8 @@ public abstract class Entity : MonoBehaviour
             _currentHealth = 0;
             DeathAnimation();
         }
+
+        HandleAfterDamage();
     }
 
     public void HealEntity(int heal)
@@ -52,10 +54,24 @@ public abstract class Entity : MonoBehaviour
             _currentHealth = _maxHealth;
         }
     }
+    
+    protected bool DamageCooldown(DamageDealerType damageDealerType)
+    {
+        // If cooldown data exist, check if it passed
+        if (_damageCooldownDict.TryGetValue(damageDealerType, out float cooldownEnd))
+        {
+            if (cooldownEnd > Time.time) return false;
+        }
+        // If it doesn't exist, or it passed
+        _damageCooldownDict[damageDealerType] = Time.time + _damageCooldownDuration;
+        return true;
+    }
 
     /* Abstract functions */
-
+    
+    protected abstract void HandleAfterDamage();
+    
     protected abstract void DeathAnimation();
 
-    protected abstract bool checkIfVulnerable(DamageDealerType damageDealerType);
+    protected abstract bool CheckIfVulnerable(DamageDealerType damageDealerType);
 }
