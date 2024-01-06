d438de2cb2b0afc8b28a502460462dfa4ab3e946
Norbert Hańderek <morph.webdev@gmail.com>
2024-01-06 19:15:08 +0100

    Fix physics accuracy for bullet triggers

--- Assets/Scripts/Damage/Projectile.cs
+++ Assets/Scripts/Damage/Projectile.cs
@@ -6,6 +6,12 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private int _damage = 5;
     [SerializeField] private Entity.DamageDealerType _damageDealerType;
+    [SerializeField] private float _lifespan = 10;
+
+    public void Start()
+    {
+        Destroy(gameObject, _lifespan);
+    }
     
     private void OnTriggerEnter(Collider other)
     {
