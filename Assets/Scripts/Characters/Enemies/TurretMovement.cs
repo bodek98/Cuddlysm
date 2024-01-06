f89b6b0e04794bc944178ab4bdeacefb0d4053d5
bodek98 <bodzio.entire@gmail.com>
2024-01-06 19:10:28 +0100

    Turret aiming higher

--- Assets/Scripts/Characters/Enemies/TurretMovement.cs
+++ Assets/Scripts/Characters/Enemies/TurretMovement.cs
@@ -7,10 +7,9 @@ public class TurretMovement : MonoBehaviour
     private Transform _playerTransform;
     private float _distance;
 
-    [SerializeField] private float _rotationSpeed = 1.0f;
     [SerializeField] private GameObject _head;
     [SerializeField] private GameObject _weapon;
-    [SerializeField] private float _gunHeight = 2.25f;
+    [SerializeField] private float _rotationSpeed = 1.0f;
     [SerializeField] private float _aggroDistance;
 
     void Start()
@@ -25,8 +24,9 @@ public class TurretMovement : MonoBehaviour
         if (_distance <= _aggroDistance) {
             float singleStep = _rotationSpeed * Time.deltaTime;
 
-            Vector3 targetHeadDirection = _playerTransform.position - _head.transform.position;
-            Vector3 targetWeaponDirection = _playerTransform.position - _weapon.transform.position;
+            Vector3 playerHeadPostion = _playerTransform.position + new Vector3(0, 1.5f, 0);
+            Vector3 targetHeadDirection = playerHeadPostion - _head.transform.position;
+            Vector3 targetWeaponDirection = playerHeadPostion - _weapon.transform.position;
 
             Vector3 newHeadDirection = Vector3.RotateTowards(_head.transform.forward, targetHeadDirection, singleStep, 0.0f);
             Vector3 newWeaponDirection = Vector3.RotateTowards(_weapon.transform.forward, targetWeaponDirection, singleStep, 0.0f);
