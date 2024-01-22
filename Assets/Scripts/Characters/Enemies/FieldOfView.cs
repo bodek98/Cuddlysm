c32cb637ed0734f952ac76c3d835a2d692ff4a35
bodek98 <bodzio.entire@gmail.com>
2024-01-22 20:51:46 +0100

    Editor drawing FOV

--- Assets/Scripts/Characters/Enemies/FieldOfView.cs
+++ Assets/Scripts/Characters/Enemies/FieldOfView.cs
@@ -2,22 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FiledOfView : MonoBehaviour
+public class FieldOfView : MonoBehaviour
 {
-    private GameObject _player;
+    public GameObject player;
 
     [Range(0, 360)]
-    [SerializeField] private float _angle;
-    [SerializeField] private float _radius;
+    public float angle;
+    public float radius;
+    public bool isPlayerVisible;
 
     [SerializeField] private LayerMask _targetMask;
     [SerializeField] private LayerMask _obstructionMask;
 
-    [SerializeField] private bool _isPlayerVisible;
 
     void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(player);
         StartCoroutine(FOVRoutine());
     }
 
@@ -34,31 +35,31 @@ public class FiledOfView : MonoBehaviour
 
     private void FieldOfViewCheck()
     {
-        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, _radius, _targetMask);
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, _targetMask);
 
         if (rangeChecks.Length != 0)
         {
             Transform target = rangeChecks[0].transform;
             Vector3 directionToTarget = (target.position - transform.position).normalized;
 
-            if (Vector3.Angle(transform.position, directionToTarget) < _angle / 2)
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
             {
-                float distanceToTarget = Vector3.Distance(transform.position, directionToTarget);
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
                 if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, _obstructionMask))
                 {
-                    _isPlayerVisible = true;
+                    isPlayerVisible = true;
                 } else
                 {
-                    _isPlayerVisible = false;
+                    isPlayerVisible = false;
                 }
             } else
             {
-                _isPlayerVisible = false;
+                isPlayerVisible = false;
             }
-        } else if (_isPlayerVisible)
+        } else if (isPlayerVisible)
         {
-            _isPlayerVisible = false;
+            isPlayerVisible = false;
         }
     }
 }
