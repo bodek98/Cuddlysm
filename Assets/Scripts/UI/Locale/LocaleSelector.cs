84eec39e55a982c02e06f2c92592a9cc10e9d442
Artur <bodzio.entire@gmail.com>
2024-12-14 17:51:48 +0100

    Localization selector script

---
+++ Assets/Scripts/UI/Locale/LocaleSelector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Localization.Settings;
+
+public class LocaleSelector : MonoBehaviour
+{
+    private void Start()
+    {
+        int ID = PlayerPrefs.GetInt("LocaleKey", 0);
+        ChangeLocale(ID);
+    }
+
+    private bool active = false;
+
+    public void ChangeLocale(int _localeID)
+    {
+        if (active == true) return;
+        Debug.Log("dupa");
+
+        StartCoroutine(SetLocale(_localeID));
+    }
+
+    IEnumerator SetLocale(int _localeID)
+    {
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        active = false;
+    }
+}
